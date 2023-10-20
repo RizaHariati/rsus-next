15176ec250fd4556a976825e445b0a4ee3ad0be7
Riza Hariati <76550345+RizaHariati@users.noreply.github.com>
2023-10-20 09:53:43 +0700

    fixing getPatient

--- sanity/sanityUtils/getPatient.ts
+++ sanity/sanityUtils/getPatient.ts
@@ -5,11 +5,12 @@ import {
   sampleSchedule,
 } from "@/app/(tools)/data/sample";
 import { NEXT_PUBLIC_BASE_URL } from "../env";
+import { PatientType } from "@/app/(tools)/patientTypes";
 const URL_PATIENT = `${NEXT_PUBLIC_BASE_URL}/api/patient/?`;
 export async function getPatient(
   medicalRecordNumber: string,
   password?: string
-): Promise<any[] | null> {
+): Promise<any> {
   console.log(
     `${URL_PATIENT}search=${medicalRecordNumber}&password=${password}`
   );
@@ -23,19 +24,17 @@ export async function getPatient(
     return data;
   } else if (res.status === 404 || res.status === 403 || res.status === 405) {
     if (medicalRecordNumber === "US4234123398") {
-      return [
-        {
-          medical_record_number: "US4234123398",
-          patient_profile: sampleProfile,
-          scheduled_appointments: sampleSchedule,
-          medical_records: [],
-          notifications: sampleNotifications,
-        },
-      ];
+      return {
+        medical_record_number: "US4234123398",
+        patient_profile: sampleProfile,
+        scheduled_appointments: sampleSchedule,
+        medical_records: [],
+        notifications: sampleNotifications,
+      };
     } else {
-      return [];
+      return {};
     }
   } else {
-    return [];
+    return {};
   }
 }
