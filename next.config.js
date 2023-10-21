63ace98bc2dce0501d5f22034e4c273e83a910d6
Riza Hariati <76550345+RizaHariati@users.noreply.github.com>
2023-10-21 14:19:12 +0700

    adding headers and removing export

--- next.config.js
+++ next.config.js
@@ -23,7 +23,7 @@ const nextConfig = {
       },
     ];
   },
-  output: process.env.NODE_ENV === "production" ? "export" : "standalone",
+  // output: process.env.NODE_ENV === "production" ? "export" : "standalone",
   reactStrictMode: true,
   // experimental: {
   //   serverActions: true,
