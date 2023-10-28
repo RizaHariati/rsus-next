f7c9415b7b4cdcf8d014d257b19c42f8fa8977c9
Riza Hariati <76550345+RizaHariati@users.noreply.github.com>
2023-10-28 07:44:35 +0700

    otak atik middleware

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,18 @@
 import { NextResponse } from "next/server";
 
 import type { NextRequest } from "next/server";
-import { NEXT_PUBLIC_BASE_URL } from "./sanity/env";
+import { NEXT_PUBLIC_BASE_URL, NEXT_PUBLIC_CONSUMER_URL } from "./sanity/env";
 
 export function middleware(req: NextRequest) {
   const url = req.nextUrl;
   const { pathname } = url;
 
   if (pathname.startsWith(`/api/`)) {
-    if (!req.headers.get("referer")?.includes(NEXT_PUBLIC_BASE_URL as string)) {
+    if (
+      !req.headers
+        .get("referer")
+        ?.includes(NEXT_PUBLIC_BASE_URL || (NEXT_PUBLIC_CONSUMER_URL as string))
+    ) {
       return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
     }
   }
--- sanity/env.ts
+++ sanity/env.ts
@@ -16,6 +16,8 @@ export const SANITY_DEPLOY_TOKEN = process.env.SANITY_DEPLOY_TOKEN;
 export const SANITY_READ_WRITE_TOKEN = process.env.SANITY_READ_WRITE_TOKEN;
 
 // export const NEXT_PUBLIC_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+export const NEXT_PUBLIC_CONSUMER_URL =
+  "https://rsuripsumoharjo-model.netlify.app";
 
 export const NEXT_PUBLIC_BASE_URL =
   process.env.NODE_ENV === "development"
