cdd58d41e384c67351e0d44ed7e32a40cc90be65
Riza Hariati <76550345+RizaHariati@users.noreply.github.com>
2023-11-22 12:53:35 +0700

    fixing loading icon

--- app/(site)/loading.tsx
+++ app/(site)/loading.tsx
@@ -18,19 +18,16 @@ const LoadingHome = (props: Props) => {
 
   return (
     <div className="page-main-container">
-      <main className=" flex-center-center flex-col p-5 text-center">
+      <main className=" flex-center-center flex-col p-5 text-center gap-5">
         <h1 className=" font-oswald">Loading </h1>
-        <div className="grid grid-cols-2 gap-5 w-full max-w-2xl">
-          <div className="flex-center-center h-[calc(100vh-168px)] w-full">
-            <GridLoader
-              color="#007814"
-              loading={loading}
-              size={10}
-              aria-label="Loading Spinner"
-              data-testid="loader"
-            />
-          </div>
-        </div>
+
+        <GridLoader
+          color="#007814"
+          loading={loading}
+          size={10}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
       </main>
     </div>
   );
